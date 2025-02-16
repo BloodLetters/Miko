5baff3aa01d264185bfd90644471ae6d81347e1f
BloodLetters <bloodletters45@gmail.com>
2025-02-16 22:19:05 +0800

    Changes to be committed: 	modified:   src/setupProxy.js 	modified:   vercel.json

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,14 +1,14 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-    app.use(
-        '/api/komiku',
-        createProxyMiddleware({
-          target: 'https://komiku.id',
-          changeOrigin: true,
-          pathRewrite: (path) => path.replace(/^\/api\/komiku/, ''),
-        })
-      );
+  app.use(
+    '/api/komiku',
+    createProxyMiddleware({
+      target: 'https://komiku.id',
+      changeOrigin: true,
+      pathRewrite: { '^/api/komiku': '' },
+    })
+  );
 
   app.use(
     '/api/komiku-api',
